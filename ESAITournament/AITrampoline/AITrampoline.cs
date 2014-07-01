97bcf7f8855697e758a536eda39cb64c4f69dacc
Drew Crawford <drew@sealedabstract.com>
2014-07-01 18:48:34 -0500

    documenting IEnumerator use

--- ESAITournament/AITrampoline/AITrampoline.cs
+++ ESAITournament/AITrampoline/AITrampoline.cs
@@ -2,8 +2,45 @@
 using System.Diagnostics;
 
 namespace AITrampoline
-{
+{	public class ESAISampleAI {
+
+	}
 	public class NotAIMaster : AI_Master {
+
+		/**It's worth talking a bit about how this works.
+		 * 
+		 * Basically Unity adds coroutines to C#.  This feature works at the compiler level--
+		 * the coroutines are translated to an IEnumerator at compile-time.  See this doc: http://wiki.etc.cmu.edu/unity3d/index.php/Coroutines
+		 * 
+		 * The net result of this is you get this whacky "sealed class <strange-name>" stuff when you reverse-engineer ES, and 
+		 * there seem to be functions returning IEnumerators everywhere in the API.  The way to read this
+		 * is the IEnumerator class is part of the function definition.  Calling MoveNext() gives the function a chance to execute.
+		 * If you return true, MoveNext() will be called again.  If you return false, it's like saying your function is done executing.
+		 * 
+		 * I think this happens because Unity is single-threaded, so they can't allow any one task to lock the whole UI.  Instead
+		 * these tasks have to stop regularly and return control to the caller, using Unity's "yield" keyword, and then are resumed. 
+		 * But that gets turned into an IEnumerator at compile.
+		 * */
+		private sealed class iterator : System.Collections.IEnumerator{
+			internal object current = null;
+			public bool MoveNext() {
+				Trace.WriteLine ("NotAIMaster moveNext");
+				return false;
+			}
+			public void Reset() {
+				throw new NotSupportedException();
+			}
+			object System.Collections.IEnumerator.Current  {
+				get {
+					return current;
+				}
+			}
+
+		}
+		public override System.Collections.IEnumerator ProcessAsync ()
+		{
+			return new iterator ();
+		}
 		public NotAIMaster(Empire e) : base(e) {
 			this.layers = new System.Collections.Generic.List<AILayer> (); //clear any layers that are present
 		}
@@ -14,6 +51,11 @@ namespace AITrampoline
 		public NotAIEmpire(Empire e) : base(e) {
 			this.layers = new System.Collections.Generic.List<AILayer> (); //clear any layers that are present
 		}
+		protected override System.Collections.IEnumerator BeginAIProcess ()
+		{
+			return base.BeginAIProcess ();
+		}
+
 	}
 	public class AITrampoline : AIPlayerController
 	{
