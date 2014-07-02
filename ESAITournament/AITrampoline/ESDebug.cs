df455dad9ed6e240a12029257c26bec54fd58f88
Drew Crawford <drew@sealedabstract.com>
2014-07-01 21:25:29 -0500

    drukenwalk proof of concept

--- ESAITournament/AITrampoline/ESDebug.cs
+++ ESAITournament/AITrampoline/ESDebug.cs
@@ -12,6 +12,13 @@ namespace AITrampoline
 		public static void debug(Mail m) {
 			Trace.WriteLine("Mail object from " + m.From + " to " + m.To + " priority " + m.Priority + " about " + m.Subject + " on " + m.Time + " saying " + m.Body);
 		}
+		public static void debug(StarSystem s) {
+			if (s==null) {
+				Trace.WriteLine ("Null starsystem");
+				return;
+			}
+			Trace.WriteLine ("Starsystem object " + s.LocalizedName);
+		}
 
 		public static void Report(Exception ex) {
 			Trace.WriteLine ("AITrampoline caught an error." + ex);
--- ESAITournament/DrunkenWalkAI/DrunkenWalkAI.cs
+++ ESAITournament/DrunkenWalkAI/DrunkenWalkAI.cs
@@ -23,12 +23,14 @@ namespace DrunkenWalkAI
 			return false; // all done
 		}
 		void drunkenWalk(Fleet f) {
-			Trace.WriteLine ("drunkenWalk on fleet " + f);
+			Trace.WriteLine ("drunkenWalk on fleet " + f + " Located at ");
+			AITrampoline.ESDebug.debug (f.Orbit);
 			_empire.DepartmentOfTransportation.ClearNextDestinationsInstruction(new Fleet[]{f});
 			//get a destination
 			System.Collections.Generic.List<StarSystem> systems =_empire.DepartmentOfTheInterior.GetVisibleStarSystems ();
 			StarSystem next = systems [new Random ().Next (systems.Count)];
-			Trace.WriteLine ("Sending to system " + next);
+			Trace.WriteLine ("Fleet sent to system");
+			AITrampoline.ESDebug.debug (next);
 			_empire.DepartmentOfTransportation.MoveFleetInstruction (f, next, PathMethod.GameRules, null, true, false);
 		}
 
